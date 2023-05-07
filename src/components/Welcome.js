d4e84b76f2f2f8e44699e4d6599734b9c8633ef8
Rohit511 <rathorerohit511@gmail.com>
2023-05-07 10:17:34 +0530

    mes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,8 @@
 // write code for Welcome component here
+const Welcome=({name})=>{
+return (<><h1>Hey{name}!
+    </h1>
+    <h2>Welcome to Newton School.</h2>
+    </>)
+}
+export default Welcome;
